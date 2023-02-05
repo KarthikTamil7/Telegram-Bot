a4a0e990ba78b520ca8301f0d3c41b2faa8a4811
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-02-05 08:46:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,7 @@ class Config:
     LIMIT = int(os.environ.get("LIMIT", "25000"))
     SKIP_NO = int(os.environ.get("SKIP_NO", "0"))
     FILTER_TYPE = os.environ.get("FILTER_TYPE", "")
+
+    # FOR AUTOMATICALLY FORWARDING MESSAGES
+    FROM_CHATS = [-1001234704297]  # List of chat id's to forward messages from
+    TO_CHATS = [-1001128355490]  # List of chat id's to forward messages to

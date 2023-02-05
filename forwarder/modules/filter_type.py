2ed9bc7e4c6fc33cfeff890e034fa541ff345ae0
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-02-05 08:37:17 +0530

    Update filter_type.py

--- forwarder/modules/filter_type.py
+++ forwarder/modules/filter_type.py
@@ -9,6 +9,20 @@ FILTER = Config.FILTER_TYPE
 FROM = Config.FROM_CHATS
 TO = Config.TO_CHATS
 
+@Client.on_message(filters.private & filters.command(["run"]))
+async def run(bot, message):
+    if str(message.from_user.id) not in Config.OWNER_ID:
+        return
+    buttons = [[
+        InlineKeyboardButton('🚫 STOP', callback_data='stop_btn')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    m = await bot.send_message(
+        text="<i>File Forwording Started😉</i>",
+        reply_markup=reply_markup,
+        chat_id=message.chat.id
+    )
+
     files_count = 0
     async for message in bot.USER.search_messages(chat_id=FROM,offset=Config.SKIP_NO,limit=Config.LIMIT,filter=FILTER):
         try:
@@ -34,3 +48,12 @@ TO = Config.TO_CHATS
         except Exception as e:
             print(e)
             pass
+   # await m.delete()
+    buttons = [[
+        InlineKeyboardButton('📜 Support Group', url='https://t.me/cinemapranthanmaar')
+    ]] 
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await m.edit(
+        text=f"<u><i>Successfully Forwarded</i></u>\n\n<b>Total Forwarded Files:-</b> <code>{files_count}</code> <b>Files</b>\n<b>Thanks For Using Me❤️</b>",
+        reply_markup=reply_markup
+    )

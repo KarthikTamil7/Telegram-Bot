818ee222eb4934685c971bad47896817cb174ad9
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-02-05 08:11:04 +0530

    Create filter_type.py

---
+++ forwarder/modules/filter_type.py
@@ -0,0 +1,36 @@
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.errors import FloodWait
+from sample_config import Config
+from translation import Translation
+
+FILTER = Config.FILTER_TYPE
+FROM = Config.FROM_CHANNEL
+TO = Config.TO_CHANNEL
+
+    files_count = 0
+    async for message in bot.USER.search_messages(chat_id=FROM,offset=Config.SKIP_NO,limit=Config.LIMIT,filter=FILTER):
+        try:
+            if message.video:
+                file_name = message.video.file_name
+            elif message.document:
+                file_name = message.document.file_name
+            elif message.audio:
+                file_name = message.audio.file_name
+            else:
+                file_name = None
+            await bot.copy_message(
+                chat_id=TO,
+                from_chat_id=FROM,
+                parse_mode="md",       
+                caption=Translation.CAPTION.format(file_name),
+                message_id=message.message_id
+            )
+            files_count += 1
+            await asyncio.sleep(1)
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+        except Exception as e:
+            print(e)
+            pass
